b7f116f84c031d4468a3a0b8b25e99b3093b698c
Kailane Sarah <kailanesarahpro@gmail.com>
2025-06-26 16:00:16 -0300

    fix(brand): corrigir campos e adicionar novos parâmetros no model Brand

--- brand/models.py
+++ brand/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 class Brand(models.Model):
-    name = models.CharField()
-    description = models.TextField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Brand'
+        verbose_name_plural = 'Brands'
+
     def __str__(self):
         return self.name
-    
