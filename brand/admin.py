a0c309500986cc0540a4b2802bf789f748165c59
Kailane Sarah <kailanesarahpro@gmail.com>
2025-06-26 16:01:50 -0300

    feat(brand): configurar visualização do modelo Brand no admin

--- brand/admin.py
+++ brand/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Brand
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')       
+    search_fields = ('name',)                                 
+    list_filter = ('created_at', 'updated_at')                
+    ordering = ('name',)                                      
+    readonly_fields = ('created_at', 'updated_at')            
+
